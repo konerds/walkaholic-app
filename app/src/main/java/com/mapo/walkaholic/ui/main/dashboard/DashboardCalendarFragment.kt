597d6f67ed4e0fdfd90a21db2fa020f6eab8d36c
jjw247 <adr10won@gmail.com>
2021-05-13 01:48:59 +0900

    20210513.

--- app/src/main/java/com/mapo/walkaholic/ui/main/dashboard/DashboardCalendarFragment.kt
+++ app/src/main/java/com/mapo/walkaholic/ui/main/dashboard/DashboardCalendarFragment.kt
@@ -24,7 +24,6 @@ import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.CalendarMode
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -36,39 +35,49 @@ class DashboardCalendarFragment :
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
 
-        val selectDay: DateFormat = SimpleDateFormat("yyyyMMddEE")
-
         var startTimeCalendar = Calendar.getInstance()
         var endTimeCalendar = Calendar.getInstance()
 
-        val currentYear = startTimeCalendar.get(Calendar.YEAR)
-        //val currentMonth = startTimeCalendar.get(Calendar.MONTH)
-        //val currentDate = startTimeCalendar.get(Calendar.DATE)
+        var currentYear = startTimeCalendar.get(Calendar.YEAR)
+        var currentMonth = startTimeCalendar.get(Calendar.MONTH)
+        var currentDate = Date()
 
         // 오늘 날짜 선택된 상태로
-        binding.calendarView.setSelectedDate(CalendarDay.today())
+        binding.calendarView.selectedDate = CalendarDay.today()
 
         var currentTime = Calendar.getInstance().getTime()
-        binding.textView.setText(SimpleDateFormat("MM월dd일, EE요일", Locale.KOREAN).format(currentTime))
+        binding.textView.setText(
+            SimpleDateFormat(
+                "MM월dd일, EE요일",
+                Locale.KOREAN
+            ).format(currentTime)
+        )
 
         // 기록 날짜 표시
         var calendarDays = arrayListOf<CalendarDay?>()
-        viewModel.calendarResponse.observe(viewLifecycleOwner,Observer { it5 ->
-            when(it5) {
+        viewModel.calendarMonthResponse.observe(viewLifecycleOwner, Observer {
+            when (it) {
                 is Resource.Success -> {
-                    var calendarDays = arrayListOf<CalendarDay?>()
-                    it5.value.walkRecord.forEachIndexed { index, walkRecord ->
+                    it.value.walkRecord.forEachIndexed { index, walkRecord ->
                         Log.e(TAG, walkRecord.toString())
-                        var year: Int = (walkRecord.walk_date?.substring(0,4))?.toInt() ?: -1
-                        var month: Int = (walkRecord.walk_date?.substring(4,6))?.toInt() ?: -1
-                        var dayy: Int = (walkRecord.walk_date?.substring(6,8))?.toInt() ?: -1
-                        calendarDays.add(CalendarDay.from(year, month-1, dayy))
+                        calendarDays.add(walkRecord.recordYear?.let { it1 ->
+                            walkRecord.recordMonth?.let { it2 ->
+                                walkRecord.recordDay?.let { it3 ->
+                                    CalendarDay.from(
+                                        it1.toInt(),
+                                        it2.toInt()-1,
+                                        it3.toInt()
+                                    )
+                                }
+                            }
+                        })
+                        binding.calendarView.addDecorator(EventDayDecorator(requireContext(), calendarDays))
                     }
                 }
                 is Resource.Loading -> {
                 }
                 is Resource.Failure -> {
-                    handleApiError(it5)
+                    handleApiError(it)
                 }
             }
         })
@@ -86,9 +95,14 @@ class DashboardCalendarFragment :
         // 달력 범위 지정
         binding.calendarView.state().edit()
             .setFirstDayOfWeek(Calendar.SUNDAY)
-            .setMinimumDate(CalendarDay.from(currentYear-20, 1, 1))
-            .setMaximumDate(CalendarDay.from(currentYear+20, 12, endTimeCalendar.getActualMaximum(
-                Calendar.DAY_OF_MONTH)))
+            .setMinimumDate(CalendarDay.from(currentYear - 20, 1, 1))
+            .setMaximumDate(
+                CalendarDay.from(
+                    currentYear + 20, 12, endTimeCalendar.getActualMaximum(
+                        Calendar.DAY_OF_MONTH
+                    )
+                )
+            )
             .setCalendarDisplayMode(CalendarMode.MONTHS)
             .commit()
 
@@ -96,79 +110,112 @@ class DashboardCalendarFragment :
         binding.calendarView.addDecorators(
             CalendarDayDecorator(),
             TodayDecorator(),
-            SeletedDayDecorator(requireContext()),
-            EventDayDecorator(requireContext(), calendarDays)
+            SeletedDayDecorator(requireContext())
         )
 
         // 달에 따라 4, 5, 6주 변동 처리
         binding.calendarView.setDynamicHeightEnabled(true)
 
-        // 날짜 선택 리스너
-        binding.calendarView.setOnDateChangedListener { widget, date, selected ->
-            binding.textView.setText(SimpleDateFormat("MM월dd일, EE요일", Locale.KOREAN).format(date.date))
-
-            viewModel.userResponse.observe(viewLifecycleOwner, Observer {
-                when(it) {
-                    is Resource.Success -> {
-                        viewModel.getCalendar(it.value.user.id, SimpleDateFormat("yyyyMMdd", Locale.KOREAN).format(date.date))
-
-                        viewModel.calendarResponse.observe(viewLifecycleOwner, Observer { it2 ->
-                            binding.dashCalendarRV.also { it3 ->
-                                val linearLayoutManager =
-                                    LinearLayoutManager(requireContext())
-                                linearLayoutManager.orientation =
-                                    LinearLayoutManager.VERTICAL
-                                it3.layoutManager = linearLayoutManager
-                                it3.setHasFixedSize(true)
-                                when(it2) {
-                                    is Resource.Success -> {
-                                        if(!it2.value.error) {
-                                            if(CalendarDay.today().date == date.date) {
-                                                binding.dashCalendarTvIntroDate.text = "오늘의 총 산책기록이에요"
-                                                binding.dashCalendarTvDetailRecord.text = "오늘의 세부 산책기록이에요"
-                                            } else {
-                                                binding.dashCalendarTvIntroDate.text = SimpleDateFormat("yyyy.MM.dd EE요일", Locale.KOREAN).format(date.date) + "의 총 산책기록이에요"
-                                                binding.dashCalendarTvDetailRecord.text = SimpleDateFormat("yyyy.MM.dd EE요일", Locale.KOREAN).format(date.date) + "의 세부 산책기록이에요"
-                                            }
-                                            it3.adapter = it2.value.walkRecord?.let { it4 ->
-                                                DashboardCalendarAdapter(it4)
-                                            }
-                                            binding.dashCalendarTvTotalDate.text = SimpleDateFormat("yyyy.MM.dd EE요일,", Locale.KOREAN).format(date.date)
-                                            var totalTime = 0
-                                            var totalDistance = 0
-                                            var totalCalorie = 0
-                                            var totalWalkAmount = 0
-                                            it2.value.walkRecord.forEachIndexed { index, walkRecord ->
-                                                totalTime += walkRecord.walk_time?.toInt() ?: -1
-                                                totalDistance += walkRecord.walk_distance?.toInt()
-                                                    ?: -1
-                                                totalCalorie += walkRecord.walk_calorie?.toInt() ?: -1
-                                            }
-                                            binding.dashCalendarTvTotalTime.text = totalTime.toString()
-                                            binding.dashCalendarTvTotalDistance.text = totalDistance.toString()
-                                            binding.dashCalendarTvCalorie.text = totalCalorie.toString()
-                                            binding.dashCalendarTvWalkAmount.text = "200"
+        viewModel.userResponse.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is Resource.Success -> {
+                    // 월 선택 리스너
+                    binding.calendarView.setOnMonthChangedListener { widget, date ->
+                        currentMonth = date.month
+                    }
+                    val _currentMonth = if (currentMonth in 1..9) {
+                        "0${currentMonth+1}"
+                    } else {
+                        (currentMonth+1).toString()
+                    }
+                    // 날짜 선택 리스너
+                    binding.calendarView.setOnDateChangedListener { widget, date, selected ->
+                        Log.e(TAG, date.date.toString())
+                        binding.textView.text = SimpleDateFormat(
+                            "MM월dd일, EE요일",
+                            Locale.KOREAN
+                        ).format(date.date)
+                        currentDate = date.date
+                        viewModel.getCalendarDate(
+                            it.value.user.id,
+                            SimpleDateFormat("yyyyMMdd", Locale.KOREAN).format(date.date)
+                        )
+                    }
+                    viewModel.getCalendarMonth(
+                        it.value.user.id, _currentMonth)
+                    viewModel.calendarResponse.observe(viewLifecycleOwner, Observer { it2 ->
+                        binding.dashCalendarRV.also { it3 ->
+                            val linearLayoutManager =
+                                LinearLayoutManager(requireContext())
+                            linearLayoutManager.orientation =
+                                LinearLayoutManager.VERTICAL
+                            it3.layoutManager = linearLayoutManager
+                            it3.setHasFixedSize(true)
+                            when (it2) {
+                                is Resource.Success -> {
+                                    if (!it2.value.error) {
+                                        if (CalendarDay.today().date == currentDate) {
+                                            binding.dashCalendarTvIntroDate.text =
+                                                "오늘의 총 산책기록이에요"
+                                            binding.dashCalendarTvDetailRecord.text =
+                                                "오늘의 세부 산책기록이에요"
+                                        } else {
+                                            binding.dashCalendarTvIntroDate.text =
+                                                SimpleDateFormat(
+                                                    "yyyy.MM.dd EE요일",
+                                                    Locale.KOREAN
+                                                ).format(currentDate) + "의 총 산책기록이에요"
+                                            binding.dashCalendarTvDetailRecord.text =
+                                                SimpleDateFormat(
+                                                    "yyyy.MM.dd EE요일",
+                                                    Locale.KOREAN
+                                                ).format(currentDate) + "의 세부 산책기록이에요"
                                         }
-                                    }
-                                    is Resource.Loading -> {
-
-                                    }
-                                    is Resource.Failure -> {
-                                        handleApiError(it2)
+                                        it3.adapter = it2.value.walkRecord?.let { it4 ->
+                                            DashboardCalendarAdapter(it4)
+                                        }
+                                        binding.dashCalendarTvTotalDate.text = SimpleDateFormat(
+                                            "yyyy.MM.dd EE요일,",
+                                            Locale.KOREAN
+                                        ).format(currentDate)
+                                        var totalTime = 0
+                                        var totalDistance = 0
+                                        var totalCalorie = 0
+                                        var totalWalkAmount = 0
+                                        it2.value.walkRecord.forEachIndexed { index, walkRecord ->
+                                            totalTime += walkRecord.walk_time?.toInt() ?: -1
+                                            totalDistance += walkRecord.walk_distance?.toInt()
+                                                ?: -1
+                                            totalCalorie += walkRecord.walk_calorie?.toInt()
+                                                ?: -1
+                                        }
+                                        binding.dashCalendarTvTotalTime.text =
+                                            totalTime.toString()
+                                        binding.dashCalendarTvTotalDistance.text =
+                                            totalDistance.toString()
+                                        binding.dashCalendarTvCalorie.text =
+                                            totalCalorie.toString()
+                                        binding.dashCalendarTvWalkAmount.text = "200"
                                     }
                                 }
-                            }
-                        })
-                    }
-                    is Resource.Loading -> {
+                                is Resource.Loading -> {
 
-                    }
-                    is Resource.Failure -> {
-                        handleApiError(it)
-                    }
+                                }
+                                is Resource.Failure -> {
+                                    handleApiError(it2)
+                                }
+                            }
+                        }
+                    })
                 }
-            })
-        }
+                is Resource.Loading -> {
+
+                }
+                is Resource.Failure -> {
+                    handleApiError(it)
+                }
+            }
+        })
         viewModel.getUser()
     }
 
--- app/src/main/java/com/mapo/walkaholic/ui/main/dashboard/DashboardCalendarViewModel.kt
+++ app/src/main/java/com/mapo/walkaholic/ui/main/dashboard/DashboardCalendarViewModel.kt
@@ -4,22 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.user.UserApiClient
+import com.mapo.walkaholic.data.model.WalkRecordExistInMonth
 import com.mapo.walkaholic.data.model.response.*
 import com.mapo.walkaholic.data.network.Resource
 import com.mapo.walkaholic.data.repository.MainRepository
 import com.mapo.walkaholic.ui.base.BaseViewModel
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.math.absoluteValue
 
 class DashboardCalendarViewModel(
     private val mainRepository: MainRepository
 ) : BaseViewModel(mainRepository) {
     override fun init() {}
-    private val _calendarResponse: MutableLiveData<Resource<WalkRecordResponse>> = MutableLiveData()
+    private val _calendarDateResponse: MutableLiveData<Resource<WalkRecordResponse>> = MutableLiveData()
     val calendarResponse: LiveData<Resource<WalkRecordResponse>>
-        get() = _calendarResponse
+        get() = _calendarDateResponse
+    private val _calendarMonthResponse: MutableLiveData<Resource<WalkRecordExistInMonthResponse>> = MutableLiveData()
+    val calendarMonthResponse: LiveData<Resource<WalkRecordExistInMonthResponse>>
+        get() = _calendarMonthResponse
     private val _userResponse: MutableLiveData<Resource<UserResponse>> = MutableLiveData()
     val userResponse: LiveData<Resource<UserResponse>>
         get() = _userResponse
@@ -37,9 +38,15 @@ class DashboardCalendarViewModel(
         }
     }
 
-    fun getCalendar(userId: Long, walkDate: String) {
+    fun getCalendarDate(userId: Long, walkDate: String) {
         viewModelScope.launch {
-            _calendarResponse.value = mainRepository.getCalendar(userId, walkDate)
+            _calendarDateResponse.value = mainRepository.getCalendarDate(userId, walkDate)
+        }
+    }
+
+    fun getCalendarMonth(userId: Long, walkMonth: String) {
+        viewModelScope.launch {
+            _calendarMonthResponse.value = mainRepository.getCalendarMonth(userId, walkMonth)
         }
     }
 
