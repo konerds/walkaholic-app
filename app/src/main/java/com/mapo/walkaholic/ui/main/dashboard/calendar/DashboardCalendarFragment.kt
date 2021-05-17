2d7e84bdc6f88e2dc2c2f42ab5964c1b72020b21
bleuuue <bleuuue@gmail.com>
2021-05-17 11:31:31 +0900

    2021-05-17

--- app/src/main/java/com/mapo/walkaholic/ui/main/dashboard/calendar/DashboardCalendarFragment.kt
+++ app/src/main/java/com/mapo/walkaholic/ui/main/dashboard/calendar/DashboardCalendarFragment.kt
@@ -59,7 +59,7 @@ class DashboardCalendarFragment :
             when (it) {
                 is Resource.Success -> {
                     it.value.walkRecord.forEachIndexed { index, walkRecord ->
-                        Log.e(TAG, walkRecord.toString())
+                        Log.e("기록", walkRecord.toString())
                         calendarDays.add(walkRecord.recordYear?.let { it1 ->
                             walkRecord.recordMonth?.let { it2 ->
                                 walkRecord.recordDay?.let { it3 ->
